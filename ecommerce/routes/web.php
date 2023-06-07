31bf723f059c3753a4e324fe19bada998f466493
NurulAulyaFitrah <nurulaulya2004@gmail.com>
2023-06-07 19:40:38 +0700

    week10

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProdukController;
+
+use App\Http\Controllers\InputController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\FormInputController;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\AboutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,17 +25,37 @@ Route::get('/', function () {
 });
 
 Route::get('/salam', function () {
-    return "Selamat Siang Aul";
+    return "Selamat Siang Pa Rojul";
 });
 
 Route::get('/about', function () {
     return view('about',[
-        "nama" => "Nurul Aulya Fitrah",
+        "nama" => "Nurul Aulya",
         "umur" => 18,
-        "email" => "nurulaulya"
+        "email" => "nurulaulya2004@gmail.com"
     ]);
 });
 
 Route::get('/periksa', function () {
     return view('periksa');
 });
+
+Route::get('/input', [InputController::class, 'index']);
+Route::post('/output', [InputController::class, 'output']);
+
+Route::get('/forminput', [FormInputController::class, 'index']);
+Route::post('/formoutput', [FormInputController::class, 'formoutput']);
+
+//ini route untuk backend atau admin
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/produk', [ProdukController::class, 'index']);
+
+});
+
+Route::prefix('arsha')->group(function () {
+    Route::get('/frontend', [FrontendController::class, 'index']);
+    Route::get('/about', [AboutController::class, 'index']);
+
+});
+//ini route untuk frontend atau user
